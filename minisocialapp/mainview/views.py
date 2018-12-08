ff5b6f6758f8ab8c91761e5be0336846c709b0cc
AnakinM <mateusz.anikiej@gmail.com>
2018-12-08 17:52:46 +0100

    Changed Bootstarp CDN and created htm file for profile view

--- minisocialapp/mainview/views.py
+++ minisocialapp/mainview/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("You are in your profile")
+    return render(request, 'mainview/profile.html')
