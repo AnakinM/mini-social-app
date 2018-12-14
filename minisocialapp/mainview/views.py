31e32f2ada2443ec334a9dea87babeadc77834e1
AnakinM <mateusz.anikiej@gmail.com>
2018-12-14 23:51:18 +0100

    cleared some useless code

--- minisocialapp/mainview/views.py
+++ minisocialapp/mainview/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
     if request.user.is_authenticated:
         return render(request, 'mainview/profile.html')
