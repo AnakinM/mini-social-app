4e83a0b92cb062cfbc05e50c79806e96d54156bb
AnakinM <mateusz.anikiej@gmail.com>
2018-12-14 23:30:36 +0100

    restricted access to profile page, only if user is logged in

--- minisocialapp/mainview/views.py
+++ minisocialapp/mainview/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'mainview/profile.html')
+    if request.user.is_authenticated:
+        return render(request, 'mainview/profile.html')
+    else:
+        return redirect('home')
